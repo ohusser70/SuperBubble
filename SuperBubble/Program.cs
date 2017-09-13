b7ca02eb92854de521229eaa620fe0c899ae385c
Olivier Hüsser <olivier.husser@he-arc.ch>
2017-09-13 11:00:26 +0200

    Ajoutez des fichiers projet.

---
+++ SuperBubble/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ *  Super implémentation de Bubble Sort
+ * 
+ *  vraiment trop bonne
+ * 
+ * */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBubble
+{
+
+    class Program
+    {
+        static void initTableau(int[] tab)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < tab.Length; i++)
+                tab[i] = rnd.Next(0, 10000);
+        }
+
+        static void DisplayTableau(int[] tab)
+        {
+            for (int i = 0; i < 16; i++)
+                Console.Write($" {tab[i]}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, voici un super BubbleSort\n");
+            int[] tab = new int[1024];
+            initTableau(tab);
+            DisplayTableau(tab);
+
+            Console.ReadKey();
+        }
+    }
+}
